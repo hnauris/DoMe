6e5bd1d8c8c91bf29cc9f7f19b6179aa8bb2ffeb
hnauris <nauris.horsts@gmail.com>
2017-07-27 20:41:03 +0300

    Comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727173435) do
+ActiveRecord::Schema.define(version: 20170727173923) do
 
   create_table "augstskolas", force: :cascade do |t|
     t.string "name"
     t.string "symbol"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
