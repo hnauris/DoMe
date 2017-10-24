1b08374ede67959b4ef06ab4312bdfcdd7b6bad2
hnauris <nauris.horsts@gmail.com>
2017-10-24 12:05:35 +0300

    Started adding notifications for new or updated posts under the new tab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_last_seen_at, if: proc {user_signed_in?}
+  private
+  def set_last_seen_at
+    current_user.update_attribute(:last_seen_at => Time.now)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   end
 
   def about
+    @last_seen = current_user.last_seen_at
+    @new_posts = Post.where(:created_at > @last_seen)
+    @updated_posts = Post.where(:updated_at > @last_seen)
   end
 
   def contact
