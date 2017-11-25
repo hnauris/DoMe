a1ebd93dea14fa518d4656e37292139f127eccdd
hnauris <nauris.horsts@gmail.com>
2017-11-25 09:55:59 +0200

    Adding email confirmation & mailer v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable, :confirmable
 
   has_many :comments
   has_many :posts
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #email confirmation mailer
+  config.action_mailer.default_url_options = {:host => 'dome12.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'dome12.herokuapp.com'
+  }
 end
