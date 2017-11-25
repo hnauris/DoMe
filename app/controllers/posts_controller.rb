742b3d99ed488d912a9dd800ef31da5d8e27bffb
hnauris <nauris.horsts@gmail.com>
2017-11-25 14:06:02 +0200

    Added edit/destroy post functionality + group landing page for followers/members

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :verify_is_group_member
+  before_action :is_owner?, only: [:edit, :update, :destroy]
   def index
     @date = params[:date]
     @group = Group.find(params[:group_id])
@@ -30,13 +31,33 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+      flash[:success] = 'Post has been updated successfully!'
+    else
+      flash[:danger] = @post.errors.full_messages
+      render 'edit'
+    end
   end
 
   def show
     
   end
 
-  def update
+  
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to group_path(@group)
+    flash[:success] = 'Post has been deleted successfully!'
   end
   private
 
@@ -47,6 +68,15 @@ class PostsController < ApplicationController
     @group = Group.find(params[:group_id])
     if @group.member_list.include?(current_user.email)
       return
+    end
+  end
+  def is_owner?
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user.id == @post.user_id
+      return
+    else
+      redirect_to group_path(@group)
     end
   end
 end
